57c45618fd43d3dd59b873c18b1c4c5d871bb657
Adrian Pop <adi.pop.bv@gmail.com>
2021-10-28 17:59:30 +0300

    Added the add book panel

--- library-manager/app/src/main/java/adipopbv/librarymanager/MainActivity.kt
+++ library-manager/app/src/main/java/adipopbv/librarymanager/MainActivity.kt
@@ -3,8 +3,12 @@ package adipopbv.librarymanager
 import adipopbv.librarymanager.domain.Book
 import adipopbv.librarymanager.utils.BookListAdapter
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.ImageButton
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +16,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var booksList: ArrayList<Book>
     private lateinit var adapter: BookListAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val addBookPanel: ConstraintLayout = findViewById(R.id.add_book_panel)
+        addBookPanel.visibility = GONE
+        findViewById<FloatingActionButton>(R.id.add_book_button).setOnClickListener {
+            addBookPanel.visibility = VISIBLE
+        }
+        findViewById<ImageButton>(R.id.cancel_button).setOnClickListener {
+            // also reset fields
+            addBookPanel.visibility = GONE
+        }
+        findViewById<ImageButton>(R.id.accept_button).setOnClickListener {
+            // also reset fields
+            addBookPanel.visibility = GONE
+            // add book
+            booksList.add(Book(
+                "Tronul de Foc",
+                "Rick Riordan",
+                "Cronicile familiei Cane",
+                2,
+                480,
+                2014,
+                "Editura Arthur",
+                "Fantasy",
+                "Nimeni",
+                "Adi's room"
+            ))
+            adapter.notifyDataSetChanged()
+        }
 
         listView = findViewById(R.id.books_list)
         booksList = arrayListOf(
@@ -45,9 +78,5 @@ class MainActivity : AppCompatActivity() {
         )
         adapter = BookListAdapter(this, booksList)
         listView.adapter = adapter
-
-        findViewById<FloatingActionButton>(R.id.add_book_button).setOnClickListener {
-
-        }
     }
 }
